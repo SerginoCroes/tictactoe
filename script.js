b5d087b99189a3adb882db89705d2fb75edfa6ce
w1z13 <115799235+w1z13@users.noreply.github.com>
2022-12-15 11:18:28 +0100

    All functional

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 const gameBoard = (function() {
 
+    const boardHTML = document.querySelectorAll('.box');
     const winMatrices = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
-    let boardContent = [];    
+    let boardContent = [];
+    let winner;
+
+    boardHTML.forEach((item, index) => {
+        item.addEventListener('click', () => {if(!winner){addItem(index)}});
+    });
 
     function resetBoard(){
+        winner = false;
         boardContent = ['', '', '',
-                        '', '', '', 
+                        '', '', '',
                         '', '', ''];
     }
 
-    function addItem(pos, player){
+    function addItem(pos){
         if (boardContent[pos] === ''){
-            boardContent.splice(pos, 1, player.sign);
-            displayController.updateDisplay(boardContent);
+            boardContent.splice(pos, 1, game.activePlayer.sign);
+            displayController.updateDisplay(boardContent, boardHTML);
+            game.advanceTurn();
         }
     }
 
@@ -21,107 +29,72 @@ const gameBoard = (function() {
         for (check of winMatrices){
             console.log(`checking matrix ${check} for player ${player.sign}`);
 
-            if([boardContent[check[0]], boardContent[check[1]], boardContent[check[2]]].every(item => item === player.sign)){return true};
+            if([boardContent[check[0]], boardContent[check[1]], boardContent[check[2]]].every(item => item === player.sign)){
+                winner = true;
+                return true;
+            }
         }
     }
-    
-    return {addItem, resetBoard, checkWin};
+
+    return {resetBoard, checkWin};
 
 })();
 
-
 const displayController = (function() {
 
-    const boardHTML = document.querySelectorAll('.box');
     const msgHTML = document.querySelector('#winmsg')
 
-    function updateDisplay(boardContent){
+    function updateDisplay(boardContent, boardHTML){
         for (item in boardContent){
             boardHTML[item].innerText = boardContent[item];
         }
     }
 
-    function displayEnd(winner){
-        msgHTML.innerText = winner ? `Player ${winner.sign} won!` : 'no winner'
+    function displayTurn(player){
+        msgHTML.innerText = `Player ${player.sign}'s turn.`;
     }
 
-    return {updateDisplay, displayEnd};
+    function displayEnd(winner){
+        msgHTML.innerText = winner ? `Player ${winner.sign} won!` : 'No winner!';
+    }
+
+    return {updateDisplay, displayEnd, displayTurn};
 
 })();
-
 
 function player(sign) {
     return {sign};
 }
 
- const game = (function() {
+const game = (function() {
 
     const playerX = player('x');
     const playerO = player('o');
-    
+
+    let turnCounter = 0;
+    let activePlayer = playerX;
     let winner;
 
-     function startGame() {
-
+    function initGame(){
         gameBoard.resetBoard();
-        let player = playerX;
+        displayController.displayTurn(this.activePlayer);
+    }
 
-    }  
-
-    gameBoard.resetBoard();
-
-    let activePlayer = playerX;
-
-
-    let i = 0; //TEMP!!!!!!!
-
-    while (!winner && i < 9){
-
-        gameBoard.addItem(i, activePlayer);
-    
-        if(gameBoard.checkWin(activePlayer)){
-            winner = activePlayer;
+    function advanceTurn(){
+        if(gameBoard.checkWin(this.activePlayer)){
+            winner = this.activePlayer;
             displayController.displayEnd(winner);
+        } else if (turnCounter === 8) {
+            displayController.displayEnd(undefined);
+        } else {
+            turnCounter++;
+            this.activePlayer = this.activePlayer === playerO ? playerX : playerO;
+            displayController.displayTurn(this.activePlayer);
         }
-    
-        console.log(winner);
-    
-        activePlayer = activePlayer === playerO ? playerX : playerO;
-    
-        i++;
     }
 
+    return {initGame, advanceTurn, activePlayer};
 
+})();
 
-}); 
-
-
-game();
-
-
-
-/* const playerX = player('x');
-const playerO = player('o');
-    
-let winner;
-
-gameBoard.resetBoard();
-let activePlayer = playerX;
-
-let i = 0; //TEMP!!!!!!!
-
-while (!winner){
-
-    gameBoard.addItem(i, activePlayer);
-
-    if(gameBoard.checkWin(activePlayer)){
-        winner = activePlayer.sign;
-    }
-
-    console.log(gameBoard.checkWin(activePlayer));
-    console.log(winner);
-
-    activePlayer = activePlayer === playerO ? playerX : playerO;
-
-    i++;
-} */
+game.initGame();
